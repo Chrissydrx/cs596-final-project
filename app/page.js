2dc7564137b529d30ca10d5ef4cf238160e63810
NicolasKmbr <braun.nicolasbraun@gmail.com>
2024-04-08 17:00:12 -0700

    Add Search Form

--- app/page.js
+++ app/page.js
@@ -1,9 +1,61 @@
-import { Button } from "@/components/ui/button";
+"use client"
+ 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+ 
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+ 
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+})
 
 export default function Home() {
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+ 
+  function onSubmit(values) {
+    console.log(values)
+  }
   return (
     <main>
-      <Button variant="default">Hello World</Button>
+      <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Full Name</FormLabel>
+              <FormControl>
+                <Input placeholder="Your Name" {...field} />
+              </FormControl>
+              <FormDescription>
+                This name is used to search for your qualifications.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Search</Button>
+      </form>
+    </Form>
     </main>
   );
 }
