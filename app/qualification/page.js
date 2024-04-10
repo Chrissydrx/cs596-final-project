372737320f66ebbd9c3a79d863619a65bca1253d
Christian Matthaei <chrissy.drx@gmail.com>
2024-04-09 21:56:51 -0700

    refactor: rename studentId to studentAddress

--- app/qualification/page.js
+++ app/qualification/page.js
@@ -3,14 +3,14 @@ import UniversityQualifications from "@/lib/web3/universityQualifications";
 export default async function Page (){
 
     //Replace with query parameter
-    const studentId = "0x775e22e991fdA0E0c089c8D860e1E2CD0068D314";
+    const studentAddress = "0x775e22e991fdA0E0c089c8D860e1E2CD0068D314";
 
-    const data = await UniversityQualifications.getQualifications(studentId);
-    const name = await UniversityQualifications.getName(studentId);
+    const data = await UniversityQualifications.getQualifications(studentAddress);
+    const name = await UniversityQualifications.getName(studentAddress);
 
     return (
         <div>
-            <h1>Qualifications for {studentId}</h1>
+            <h1>Qualifications for {studentAddress}</h1>
             <h2>{name ? name : "No name provided"}</h2>
 
             <br/>
