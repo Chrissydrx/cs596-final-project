d193111a6257c094b385a1d82fb849f9727d6200
Christian Matthaei <chrissy.drx@gmail.com>
2024-04-09 21:55:13 -0700

    feature: fetch name for address

--- app/qualification/page.js
+++ app/qualification/page.js
@@ -6,13 +6,15 @@ export default async function Page (){
     const studentId = "0x775e22e991fdA0E0c089c8D860e1E2CD0068D314";
 
     const data = await UniversityQualifications.getQualifications(studentId);
+    const name = await UniversityQualifications.getName(studentId);
 
     return (
         <div>
             <h1>Qualifications for {studentId}</h1>
+            <h2>{name ? name : "No name provided"}</h2>
 
-            
-        
+            <br/>
+
             {data.length == 0 ? "No data" : data.map((qualification, index) => (
                 <div key={index}>
                     <p>{qualification.name}</p>
@@ -20,7 +22,9 @@ export default async function Page (){
                     <p>{qualification.issuedBy}</p>
                     <p>{qualification.issueDate}</p>
                     <p>{qualification.qualificationType}</p>
+                    <br/>
                 </div>
+                
             ))}
 
         </div>
