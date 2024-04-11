c4ef24be803e0714951666b993352a0555fc64e3
NicolasKmbr <braun.nicolasbraun@gmail.com>
2024-04-10 18:43:50 -0700

    Add: Qualification Enum, Issue date and qualification type

--- app/qualifications/[user]/page.js
+++ app/qualifications/[user]/page.js
@@ -8,6 +8,8 @@ import {
 } from "@/components/ui/card";
 import UniversityQualifications from "@/lib/web3/universityQualifications";
 
+import { Qualification } from "@/lib/structures";
+
 async function QualificationsPage({params}) {
 
 //0x775e22e991fdA0E0c089c8D860e1E2CD0068D314
@@ -19,19 +21,18 @@ async function QualificationsPage({params}) {
   return (
     <main className="h-screen p-10 flex flex-col gap-3">
       <h1>Qualifications for {studentAddress}</h1>
-      <h2>{name ? name : "No name provided"}</h2>
-      <h3>{params?.user || "No User provided"}</h3>
+      <h2>Name of Student: {name ? name : "No name provided"}</h2>
       {data.length == 0 ? "No data" : data.map((qualification, index) => (
       <Card key={index}>
         <CardHeader>
           <CardTitle>{qualification.name}</CardTitle>
-          <CardDescription>{qualification.issuedBy}, {qualification.issueDate ? qualification.issueDate : "No Issue Date"}</CardDescription>
+          <CardDescription>{qualification.issuedBy}, {qualification.issueDate ? new Date(qualification.issueDate.toString() * 1000).toLocaleDateString("en-US") : "No Issue Date"}</CardDescription>
         </CardHeader>
         <CardContent>
           <p>{qualification.description}</p>
         </CardContent>
         <CardFooter>
-          <p>{qualification.qualificationType}</p>
+          <p>Qualification Type: {qualification.qualificationType || qualification.qualificationType == 0 ? Qualification[qualification.qualificationType] : "No Type"}</p>
         </CardFooter>
       </Card>
       ))}
