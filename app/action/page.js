35339cd7b0118e5e2f726bbfdd24d4b7e8a51468
Christian Matthaei <chrissy.drx@gmail.com>
2024-04-29 13:31:31 -0700

    feature: logout button

--- app/action/page.js
+++ app/action/page.js
@@ -1,5 +1,12 @@
+import LogoutButton from "@/components/logout-button";
+
 function Page() {
-  return <p>Insert all action pages here</p>;
+  return (
+    <>
+      <LogoutButton className="fixed top-1 right-1" />
+      <p>Insert all action pages here</p>
+    </>
+  );
 }
 
 export default Page;
