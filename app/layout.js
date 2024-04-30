35830b55fd364b94f2881ef56d4d20f9a0fab6e8
Christian Matthaei <chrissy.drx@gmail.com>
2024-04-29 17:11:46 -0700

    feature: center content

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.className} mx-auto max-w-[1000px] min-h-screen flex flex-col justify-center`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
