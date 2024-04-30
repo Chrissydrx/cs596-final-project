b09a558a1a3a1e40111dd761a2edf113a38038b3
Christian Matthaei <chrissy.drx@gmail.com>
2024-04-29 19:37:20 -0700

    feature: vote ui

--- app/action/vote/components/address-card.js
+++ app/action/vote/components/address-card.js
@@ -1,5 +1,8 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { Card, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import LoadingScreen from "@/components/ui/loading-screen";
 import SmartContractClient from "@/lib/web3/smart-contract-client";
 
 import { useState } from "react";
@@ -27,22 +30,31 @@ function AddressCard({ applicant, setActionCount }) {
         event.preventDefault();
       }}
     >
-      <p>{applicant}</p>
-      <button
-        type="button"
-        disabled={isLoading}
-        onClick={(event) => handleSubmit(event, "Y")}
-      >
-        {isLoading ? "Loading..." : "Yes"}
-      </button>
-      <button
-        type="button"
-        disabled={isLoading}
-        onClick={(event) => handleSubmit(event, "N")}
-      >
-        {isLoading ? "Loading..." : "No"}
-      </button>
-      <hr />
+      <Card>
+        <CardHeader>
+          <CardTitle>{applicant}</CardTitle>
+        </CardHeader>
+        <CardFooter className="flex justify-center gap-4">
+          <Button
+            type="button"
+            disabled={isLoading}
+            onClick={(event) => handleSubmit(event, "Y")}
+            className="bg-green-600 hover:bg-green-700 w-[200px]"
+          >
+            Accept
+          </Button>
+          <Button
+            type="button"
+            disabled={isLoading}
+            onClick={(event) => handleSubmit(event, "N")}
+            className="bg-red-600 hover:bg-red-700 w-[200px]"
+          >
+            Reject
+          </Button>
+        </CardFooter>
+      </Card>
+      <LoadingScreen visible={isLoading} />
+      <br />
     </form>
   );
 }
